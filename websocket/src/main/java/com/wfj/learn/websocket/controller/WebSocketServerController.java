47ceca33823243de8399057aeddfc54c5d22d6b9
wfj <mr.wfj@qq.com>
2019-07-10 13:44:24 +0800

    update

---
+++ websocket/src/main/java/com/wfj/learn/websocket/controller/WebSocketServerController.java
@@ -0,0 +1,48 @@
+package com.wfj.learn.websocket.controller;
+
+import com.wfj.learn.websocket.websocket.server.WebSocketServer;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/websocket/server")
+public class WebSocketServerController {
+
+
+    /**
+     * 群发消息
+     *
+     * @param message 消息
+     * @return
+     */
+    @PostMapping("/sendAll")
+    String sendAllMessage(@RequestParam(required = true) String message) {
+        try {
+            WebSocketServer.broadCastInfo(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "ok";
+    }
+
+    /**
+     * 指定会话ID发消息
+     *
+     * @param message 消息
+     * @param id      sessionId
+     * @return String
+     */
+    @PostMapping("/sendOne")
+    String sendOneMessage(@RequestParam(required = true) String message, @RequestParam(required = true) String id) {
+        try {
+            WebSocketServer.sendMessage(id, message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "ok";
+    }
+}
